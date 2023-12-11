b98222d1a6b66c501e24ae54eb390457f3f46dc3
Lars Kellogg-Stedman <lars@redhat.com>
2023-12-11 18:08:22 -0500

    Add test framework
    This commit adds a pytest-driven suite of tests (in test_app.py) and
    generally cleans up both the code in app.py and the Dockerfile to address
    my comments on [1].
    
    [1]: https://github.com/CCI-MOC/ansible-switches-auto-deploy/pull/1

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
 FROM registry.access.redhat.com/ubi8/python-39@sha256:496f9eee1837022e749a5f2d5c9ae91720200e03c38528894aee4eb6b3bb78b5
 
-USER root
-
-RUN INSTALL_PKGS="flask ansible_runner ansible-pylibssh jsonschema GitPython requests" && \
-    pip install $INSTALL_PKGS
-
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
 COPY app.py ./
 
-CMD [ "python", "./app.py"]
+CMD [ "gunicorn", "-b", "0.0.0.0:8000", "app:app"]
--- app.py
+++ app.py
@@ -1,84 +1,112 @@
-from flask import Flask, request, abort
+from flask import Flask, request, current_app
 from git import Repo
 import ansible_runner
 import os
 import requests
 import json
-import configparser
+import logging
 
-from datetime import datetime
 
-app = Flask(__name__)
+class CONFIG_DEFAULTS:
+    COLOR_SUCCESS = "#05eb2f"
+    COLOR_FAILURE = "#f00216"
 
-def appendTime(message):
-    output = "[" + datetime.utcnow().isoformat() + "]" + " " + message
-    return (output)
 
-def updateRepo(repoName, repoURL): #Update the local git repo
-
-    if os.path.isdir(repoName): #If the clone of the repo exists pull updates, if not clone it 
-        repo = Repo(repoName)
+def updateRepo(repoDir, repoURL):
+    """Clone or update the local git repository"""
+    if os.path.isdir(
+        repoDir
+    ):  # If the clone of the repo exists pull updates, if not clone it
+        repo = Repo(repoDir)
         o = repo.remotes.origin
         o.pull()
-        print(appendTime("Repo Pulled"))
+        current_app.logger.info("pulled changes for %s", repoURL)
     else:
-        Repo.clone_from(repoURL, repoName)
-        print(appendTime("Repo Cloned"))
+        Repo.clone_from(repoURL, repoDir)
+        current_app.logger.info("clone %s", repoURL)
 
-def fileChanges(commits): #Returns bool for if vlan file changes and list of individual switch changes
-    
-    vlanChange = False #Vlan Flag
-    switchChanges = [] #List of modified switches
 
-    for commit in commits: #Look at each commit in push to main branch
-        
-        for file in commit['modified']: #Look at the files modified in each commit
-            split = file.split("/")
-            
-            if split[2] == 'vlans.yaml' : #Set flag to true if vlan change
+def fileChanges(
+    commits,
+):
+    """Returns bool for if vlan file changes and list of individual switch changes"""
+    vlanChange = False  # Vlan Flag
+    switchChanges = []  # List of modified switches
+
+    for commit in commits:  # Look at each commit in push to main branch
+        for path in commit["modified"]:  # Look at the files modified in each commit
+            if path == "group_vars/all/vlans.yaml":
                 vlanChange = True
-                
-            elif split[0] == 'host_vars' : #Add all changed switches to a list 
-                switchChanges.append(split[1]) #Adds switch name to list
 
-    print(appendTime("Vlan change: " + vlanChange + "\nSwitch chnages: " + switchChanges))
+            elif path.startswith("host_vars") and path.endswith("interfaces.yaml"):
+                switchChanges.append(path.split("/")[1])
 
-    return(vlanChange, switchChanges)
-
-def runAnsible(vlanFlag, switchList): #Function for running ansible playbook
-    if vlanFlag: #If the vlanFlag is true run playbook on all switches else only on switches with changes
-        runAll = ansible_runner.run(
-            private_data_dir='ansible-switches/',
-            playbook='deploy.yaml'
-         )
-        print(appendTime("Vlan change, all switches update."))
-        return(runAll.status, runAll.stdout)
-
-    else:
-        strSwitchList = ','.join(map(str,switchList)) #Convert the list to string, items seperated by commas
-        runSwitches = ansible_runner.run(
-            private_data_dir='ansible-switches/',
-            playbook='deploy.yaml',
-            limit= strSwitchList #equivalent to --limit
+        current_app.logger.info(
+            "Vlan change: %s Switch changes: %s", vlanChange, switchChanges
         )
-        print(appendTime("Individual switch change. " + strSwitchList + " switches update."))
-        return(runSwitches.status, runSwitches.stdout)
-    
-def sendAlert(vlanFlag, switchList, status, stdoutPath, webhook_url): #Function to send notification to slack channel
 
-    strSwitchList = ','.join(map(str,switchList)) #Convert the list to string, items seperated by commas
+    return (vlanChange, switchChanges)
 
-    if status == 'failed':
-        color = '#f00216' #red color
-        title = ("Switch Change :large_red_square:")
+
+def runAnsible(repoDir, vlanFlag, switchList):
+    """Run the deployment playbook"""
+    playbook = current_app.config.get("AUTODEPLOY_PLAYBOOK", "deploy.yaml")
+
+    if (
+        vlanFlag
+    ):  # If the vlanFlag is true run playbook on all switches else only on switches with changes
+        res = ansible_runner.run(
+            private_data_dir=repoDir,
+            playbook=playbook,
+        )
+        current_app.logger.info("Vlan change, all switches update.")
+
     else:
-        color = '#05eb2f' #green color
-        title = ("Switch Change :large_green_square:")
+        strSwitchList = ",".join(
+            map(str, switchList)
+        )  # Convert the list to string, items seperated by commas
+        res = ansible_runner.run(
+            private_data_dir=repoDir,
+            playbook=playbook,
+            limit=strSwitchList,  # equivalent to --limit
+        )
+        current_app.logger.info("Individual switch change: %s", strSwitchList)
+    return (res.status, res.stdout)
 
-    if vlanFlag: #Create the messages for the alerts, different depending on what switches were changed
-        message = 'Change made to switches.\nAll switches were changed.\nStatus:' + status + '\nPath to stdout: ' + str(stdoutPath.name)
+
+def sendAlert(vlanFlag, switchList, status, stdoutPath, webhook_url):
+    """Send notification to slack channel"""
+    strSwitchList = ",".join(
+        map(str, switchList)
+    )  # Convert the list to string, items seperated by commas
+
+    if status == "failed":
+        color = current_app.config["COLOR_FAILURE"]
+        title = "Switch Change :large_red_square:"
     else:
-        message = 'Change made to switches.\nSwitches: \n' + strSwitchList + '\nStatus: ' + status + '\nPath to stdout: ' + str(stdoutPath.name)
+        color = current_app.config["COLOR_SUCCESS"]
+        title = "Switch Change :large_green_square:"
+
+    if (
+        vlanFlag
+    ):  # Create the messages for the alerts, different depending on what switches were changed
+        # LKS: Here (and elsewhere) we should probably look into using
+        # python f-strings rather than string concatenation.
+        message = (
+            "Change made to switches.\nAll switches were changed.\nStatus: "
+            + status
+            + "\nPath to stdout: "
+            + str(stdoutPath.name)
+        )
+    else:
+        message = (
+            "Change made to switches.\nSwitches: \n"
+            + strSwitchList
+            + "\nStatus: "
+            + status
+            + "\nPath to stdout: "
+            + str(stdoutPath.name)
+        )
 
     slack_data = {
         "username": "SwitchAlert",
@@ -93,47 +121,57 @@ def sendAlert(vlanFlag, switchList, status, stdoutPath, webhook_url): #Function 
                         "value": message,
                         "short": "false",
                     }
-                ]
+                ],
             }
-        ]
+        ],
     }
 
     headers = {
-        'Content-Type': "application/json",
+        "Content-Type": "application/json",
     }
 
-    response = requests.post(
-        webhook_url,
-        data=json.dumps(slack_data),
-        headers=headers
-    )
+    response = requests.post(webhook_url, data=json.dumps(slack_data), headers=headers)
 
-    print(appendTime(response.text))
-        
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    repoName = config['AUTODEPLOY']['RepoName']
-    repoURL = config['AUTODEPLOY']['RepoURL']
-    webhookURL = config['AUTODEPLOY']['WebhookURL']
+    current_app.logger.info("notify response: %s", response.text)
 
-    if request.method == 'POST':
 
-        print(appendTime("Recieved Webhook"))
+def create_app(config_from_env=True):
+    """Create and configure flask application"""
+    app = Flask(__name__)
+    app.config.from_object(CONFIG_DEFAULTS)
 
-        vlanFlag, switchList = fileChanges(request.json['commits'])
+    if config_from_env:
+        app.config.from_prefixed_env()
+
+    @app.route("/webhook", methods=["POST"])
+    def webhook():
+        repoURL = current_app.config["AUTODEPLOY_REPOURL"]
+        repoDir = current_app.config.get(
+            "AUTODEPLOY_REPODIR", os.path.basename(repoURL)
+        )
+        webhookURL = current_app.config["AUTODEPLOY_WEBHOOKURL"]
+
+        current_app.logger.info("received webhook")
+
+        vlanFlag, switchList = fileChanges(request.json["commits"])
         if vlanFlag or switchList:
+            current_app.logger.info("changes detected")
 
-            print(appendTime("Changes detected"))
+            updateRepo(repoDir, repoURL)
+            status, stdoutPath = runAnsible(repoDir, vlanFlag, switchList)
 
-            updateRepo(repoName, repoURL)
-            status, stdoutPath = runAnsible(vlanFlag, switchList)
+            current_app.logger.info("sending notification to %s", webhookURL)
             sendAlert(vlanFlag, switchList, status, stdoutPath, webhookURL)
-        return 'success', 200
-        
-    else:
-        abort(400)
+        return "success", 200
 
-if __name__ == '__main__':
+    @app.before_request
+    def configure_logging():
+        logging.basicConfig(level=current_app.config.get("LOGLEVEL", "INFO"))
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
     app.run()
